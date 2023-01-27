07855d622203cd1b326643286de3dcd4de20d36c
muhammad bamian <mohammadmahdibamiyan@gmail.com>
2023-01-27 18:44:15 +0330

    fix bog

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -1,60 +1,58 @@
 @extends('layouts.master')
 
 @section('style')
-    
 @endsection
 @section('content')
-<div class="container py-5 mt-5">
-    <form action="{{route('books.update',['book'=>$book->id])}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="row p-5 border shadow">
-            <div class="col-md-3 border shadow mb-3 flex-column justify-content-around py-5">
-                    <img src="{{asset($book->image)}}" alt="" class="w-100">
+    <div class="container py-5 mt-5">
+        <form action="{{ route('books.update', ['book' => $book->id]) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row p-5 border shadow">
+                <div class="col-md-3 border shadow mb-3 flex-column justify-content-around py-5">
+                    <img src="{{ asset($book->image) }}" alt="" class="w-100">
                     <label for="file-upload" class="w-100">
-                        <input name="img" type="file" id="file-upload" />
+                        <input name="img" type="file" id="file-upload" hidden />
                         <span class="BTN w-100 text-center">آپلود عکس</span>
-                    </label>                       
-            </div>
-            <div class="col-md-6 border shadow mb-3">
-                <div class="p-5">
-                    <div class=" d-flex pb-4 mb-5 mt-5">
-                        <label class="pb-2">دسته بندی</label>
-                        <select name="category_id" class="form-select" aria-label="Default select example">
-                            <option value="">انتخاب دسته بندی</option>
-                            @foreach ($categories as $category)
-                                <option value="{{$category->id}}" {{($category->id==$book->category_id ? "selected" :"" )}}>{{$category->title}}</option>
-                            @endforeach
-                          </select>
-                    </div>
-                    <div class="d-flex pb-4 mb-5 mt-5">
-                        <span>عنوان کتاب</span>
-                        <input type="text" name="title" class="form-control" value="{{old('title',$book->title)}}" placeholder="عنوان کتاب">
-                    </div>                
-                    <div class="d-flex pb-4 mb-5 mt-5">
-                        <span>نام نویسنده:</span>
-                        <input type="text" name="writer" class="form-control" value="{{old('writer',$book->writer)}}" placeholder="نام نویسنده">
-                    </div>
-                    <div class="d-flex pb-4 mb-5 mt-5">
-                        <span>سال انتشار:</span>
-                        <input type="text" name="release_date" class="form-control" value="{{old('release_date',$book->release_date)}}" placeholder="سال انتشار">
-                    </div>
+                    </label>
                 </div>
-    
-            </div>
-            <div class="col-md-3 border shadow mb-3 d-flex flex-column py-5">
-                <img src="{{asset('image/blog-2.jpg')}}" alt="" class="w-100">
+                <div class="col-md-6 border shadow mb-3">
+                    <div class="p-5">
+                        <div class=" d-flex pb-4 mb-5 mt-5">
+                            <label class="pb-2">دسته بندی</label>
+                            <select name="category_id" class="form-select" aria-label="Default select example">
+                                <option value="">انتخاب دسته بندی</option>
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}" {{ $category->id == $book->category_id ? 'selected' : '' }}>{{ $category->title }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="d-flex pb-4 mb-5 mt-5">
+                            <span>عنوان کتاب</span>
+                            <input type="text" name="title" class="form-control" value="{{ old('title', $book->title) }}" placeholder="عنوان کتاب">
+                        </div>
+                        <div class="d-flex pb-4 mb-5 mt-5">
+                            <span>نام نویسنده:</span>
+                            <input type="text" name="writer" class="form-control" value="{{ old('writer', $book->writer) }}" placeholder="نام نویسنده">
+                        </div>
+                        <div class="d-flex pb-4 mb-5 mt-5">
+                            <span>سال انتشار:</span>
+                            <input type="text" name="release_date" class="form-control" value="{{ old('release_date', $book->release_date) }}" placeholder="سال انتشار">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-3 border shadow mb-3 d-flex flex-column py-5">
+                    <img src="{{ asset('image/images_300x380.jpg') }}" alt="" class="w-100">
                     <label for="file-upload1" class="w-100">
-                        <input name="book_file" type="file" id="file-upload1" />
+                        <input name="book_file" type="file" id="file-upload1" hidden />
                         <span class="BTN w-100 text-center">آپلود فایل کتاب</span>
-                    </label>                       
+                    </label>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <button class="btn btn-success btn-lg w-100 text-center" type="submit">ویرایش</button>
-        </div>
-    </form>
-</div>
+            <div class="row">
+                <button class="btn btn-success btn-lg w-100 text-center" type="submit">ویرایش</button>
+            </div>
+        </form>
+    </div>
 @endsection
 @section('script')
-    
 @endsection
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -5,38 +5,41 @@
 @endsection
 @section('content')
 <div class="container py-5 mt-5">
-    <div class="row p-5 border shadow">
-        <div class="col-md-3 border shadow mb-3 d-flex align-items-center py-5">
-                <img src="{{asset($book->image)}}" alt="" class="w-100">                                    
+    <div class="row p-5 border shadow col-8 mx-auto">        
+        <div class="col-md-3 mb-3 d-flex flex-column align-items-center py-5">
+                <img src="{{asset($book->image)}}" alt="" class="w-100">  
+                <div class="w-100">
+                    <a href="{{route('books.reserve',['book'=>$book->id])}}" class="BTN w-100 text-center">رزرو</a>
+                </div>                                  
         </div>
-        <div class="col-md-6 border shadow mb-3">
-            <div class="p-5">
+        <div class="col-md-9 mb-3">
+            <div class="p-5 d-flex flex-column">
                 <div class="d-flex pb-4">
-                    <span>عنوان کتاب</span>
-                    <span>{{$book->title}}</span>
+                    <span style="min-width: 100px">عنوان کتاب :</span>
+                    <span class="pe-5">{{$book->title}}</span>
                 </div>                
                 <div class="d-flex pb-4">
-                    <span>نویسنده:</span>
-                    <span>{{$book->writer}}</span>
+                    <span style="min-width: 100px">نویسنده:</span>
+                    <span class="pe-5">{{$book->writer}}</span>
                 </div>
                 <div class="d-flex pb-4">
-                    <span>موضوع:</span>
-                    <span>{{$book->categoryTitle}}</span>
+                    <span style="min-width: 100px">موضوع:</span>
+                    <span class="pe-5">{{$book->categoryTitle}}</span>
                 </div>
                 <div class="d-flex pb-4">
-                    <span>سال انتشار:</span>
-                    <span>{{$book->release_date}}</span>
+                    <span style="min-width: 100px">سال انتشار:</span>
+                    <span class="pe-5">{{$book->release_date}}</span>
                 </div>
             </div>
 
         </div>
-        <div class="col-md-3 border shadow mb-3 d-flex flex-column align-items-center py-5">
+        {{-- <div class="col-md-3 mb-3 d-flex flex-column align-items-center py-5">
             @if ($reserve)
                 <a href="{{route('books.download',['book'=>$book->id])}}" class="BTN">دانلود فایل</a>
             @else
                 <a href="{{route('books.reserve',['book'=>$book->id])}}" class="BTN">رزرو</a>
             @endif
-        </div>
+        </div> --}}
     </div>
 </div>
     
