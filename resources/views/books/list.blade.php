c02c84e9385f6c37b5d8d33fffd5a0f3050d3c99
alireza00027 <alirezaghotbadini@gmail.com>
2023-01-27 20:21:58 +0330

    fix bugs 13

--- resources/views/books/list.blade.php
+++ resources/views/books/list.blade.php
@@ -31,30 +31,12 @@
                     </div>
                 @endforeach
                 <div class="col-12 py-5">
-                    {{-- {!! $books->withQueryString()->links() !!} --}}
-                    <nav aria-label="Page navigation example" class="d-flex justify-content-center py-5">
-                        <ul class="pagination">
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Previous">
-                                    <span aria-hidden="true">&laquo;</span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Next">
-                                    <span aria-hidden="true">&raquo;</span>
-                                    <span class="sr-only">Next</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div class="col-12 py-5">
+                        {!! $books->withQueryString()->links() !!}
+                    </div>
                 </div>
             </div>
         </div>
-
     </section>
 @endsection
 @section('script')
